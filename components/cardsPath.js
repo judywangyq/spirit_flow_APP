6938718b7a43ff1d2ff062d3bef6d9c77e73a722
judywangyq <judywangyq@gmail.com>
2023-11-18 20:20:26 -0800

    added few cards

--- components/cardsPath.js
+++ components/cardsPath.js
@@ -1,6 +1,13 @@
 const cardsImagePath = {
+    c02: require('../assets/cards/c02.jpg'),
     c03: require('../assets/cards/c03.jpg'),
+    m00: require('../assets/cards/m00.jpg'),
+    m02: require('../assets/cards/m02.jpg'),
+    m12: require('../assets/cards/m12.jpg'),
     p01: require('../assets/cards/p01.jpg'),
+    p06: require('../assets/cards/p06.jpg'),
+    s04: require('../assets/cards/s04.jpg'),
+    w07: require('../assets/cards/w07.jpg'),
     w14: require('../assets/cards/w14.jpg'),
   };
   
