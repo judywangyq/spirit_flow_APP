4095decd39960b04f46efe4c5afb3c5ab4b821c7
yingying <yying023@outlook.com>
2023-11-19 21:05:57 -0800

    add edit function in profile page, delete some unnecessary console.log

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 import { auth, database } from "../firebase/firebaseSetup";
-import { doc, getDocs, collection, query, where } from 'firebase/firestore';
+import { getDocs, collection, query, where, updateDoc } from 'firebase/firestore';
 
 
 const UserProfile = () => {
   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+
   const fetchUserData = async (uid) => {
-    console.log("here the uid is,", uid); // here is good
     try {
       const q = query(collection(database, "users"), where("uid", "==", uid));
       const querySnapshot = await getDocs(q);
-      console.log("querySnapshot is: ", querySnapshot);
       querySnapshot.forEach((doc) => {
         const userData = doc.data();
         setFullName(userData.fullName);
@@ -46,18 +46,68 @@ const UserProfile = () => {
   }, []);
 
   const handleEditPress = () => {
-    // Add logic for handling the edit button press
-    console.log('Edit button pressed');
-    // You can navigate to another screen or show a modal for editing here
+    const originalUserData = { fullName, email };
+    setIsEditing(true);
+  };
+
+  const handleSavePress = async () => {
+    try {
+      // Fetch the user document reference
+      const q = query(collection(database, "users"), where("uid", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0].ref;
+
+        // Update the user document
+        await updateDoc(userDoc, {
+          fullName,
+          email,
+        });
+
+        console.log('User data updated successfully');
+      } else {
+        console.error('User document not found');
+      }
+    } catch (error) {
+      console.error('Error updating user data:', error);
+    } finally {
+      setIsEditing(false); // Reset the editing state
+    }
+  };
+
+  
+  const handleCancelPress = () => {
+    setIsEditing(false); 
   };
 
   return (
     <View>
-      <Text>Email: {email}</Text>
-      <Text>Full Name: {fullName}</Text>
-      <Button title="Edit" onPress={handleEditPress} />
+      {isEditing ? (
+        <View>
+          <TextInput
+            placeholder="Enter Full Name"
+            value={fullName}
+            onChangeText={(text) => setFullName(text)}
+          />
+          <TextInput
+            placeholder="Enter Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Button title="Save" onPress={handleSavePress} />
+          <Button title="Cancel" onPress={handleCancelPress} />
+        </View>
+      ) : (
+        <View>
+          <Text>Email: {email}</Text>
+          <Text>Full Name: {fullName}</Text>
+          <Button title="Edit" onPress={handleEditPress} />
+        </View>
+      )}
     </View>
   );
+  
 };
 
 export default UserProfile;
