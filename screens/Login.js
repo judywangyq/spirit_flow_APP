c547a253e45c613fc634b67eb3a2ea3511d156bd
yingying <yying023@outlook.com>
2023-11-19 22:15:54 -0800

    modify style in signup and login

--- screens/Login.js
+++ screens/Login.js
@@ -29,6 +29,8 @@ export default function Login({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>SpiritFlow</Text>
+      <View style={styles.spacing} />
       <Text style={styles.label}>Email</Text>
       <TextInput
         placeholder="Email"
@@ -59,16 +61,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "stretch",
-    justifyContent: "center",
+    paddingHorizontal: 20, 
+  },
+  title: {
+    fontSize: 50, 
+    fontWeight: "bold", 
+    // marginLeft: 10,
+    marginTop: 100, 
+    marginBottom: 10,
+    fontFamily:"Cochin",
+  },
+  spacing: {
+    marginBottom: 80,
   },
   input: {
     borderColor: "#552055",
     borderWidth: 2,
-    width: "90%",
-    margin: 5,
-    padding: 5,
+    width: "100%", 
+    marginVertical: 5,
+    padding: 10, 
   },
   label: {
     marginLeft: 10,
+    marginBottom: 5, 
+    alignSelf: "flex-start",
   },
 });
